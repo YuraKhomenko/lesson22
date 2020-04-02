2b158fa1aa7f3488628eb1b9dc4325c992b9863c
Yura Khomenko <yurakhomenko2017@gmail.com>
2020-04-02 17:38:41 +0300

    Array

---
+++ Home work2/homeWork2.js
@@ -0,0 +1,62 @@
+/*let yellow = '<img src="iphoneyellow.jpg" alt="" width="500px" height="700px"> ';
+let price = 0;
+while(true) { 
+let memory = prompt("How much memory do you want in phone?");
+    
+if (memory ===64) {
+   price = 300; 
+}
+else if (memory === 128) {
+    price = 500; 
+}
+else if (memory === 256) {
+    price = 700; 
+} 
+else {
+  if (memory === null || !+memory || memory !== 64 || memory !== 128 || memory !== 256) {
+    prompt("Число должно быть 64, 128, или 256");   
+}
+}
+}
+alert("Price is " + price + "$");
+
+let img = "iphoneblue.jpg";
+let img1 = "iphoneyellow.jpg";
+let img2 = "iphonegrey.jpg";
+let img3 = "iphonegreen.jpg";
+ document.write(`<img src="${img2}">`);
+
+
+  
+ let arr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+ for (let i = 0; i < arr.length; i++) {
+   if (arr[i] === "Tue" || arr[i] === "Thu") {
+     continue;
+   }
+   console.log(arr[i]);
+ }*/
+ price = 0;
+ let memory;
+ 
+ do {
+  memory = prompt("How much memory do you want in phone?");
+}
+while (memory === null || !+memory || memory !== 64 || memory !== 128 || memory !== 256) {
+  prompt("Число должно быть 64, 128, или 256");   {
+  }
+ if (memory === null || !+memory) {
+  alert("Wrong"); 
+ } else {
+        if (+memory === 64) {
+          price = 300; 
+       }
+       else if (+memory === 128) {
+           price = 500; 
+       }
+       else if (+memory === 256) {
+           price = 700; 
+       } 
+       }
+      
+       alert("Price is " + price + "$");
+ 
---
+++ Home work2/homeWorkArray.js
@@ -0,0 +1,10 @@
+      let arr = [];
+      let number = prompt("Введите размер массива");
+      for (let i = 0; i < number; i++){
+        arr[i] = [];
+          for (let j = 0; j < number; j++){
+              arr[i][j] = 0;
+      }
+    } 
+      console.log(arr);
+
